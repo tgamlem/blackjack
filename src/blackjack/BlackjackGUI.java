aab76821523bbf051388b0686bf022540540e447
Tyler J. Gamlem <tgamlem22@my.whitworth.edu>
2021-01-21 14:32:53 -0800

    change names of all card images

--- src/blackjack/BlackjackGUI.java
+++ src/blackjack/BlackjackGUI.java
@@ -7,14 +7,22 @@ import javax.swing.*;
 public class BlackjackGUI implements MouseMotionListener {
 	// window for the GUI
 	JFrame frame = new JFrame("Blackjack");
+	// panel to display content within
 	JPanel panel = new JPanel();
+	// background of the panel
+	JLabel background = new JLabel();
+	// text area for player to enter their bet
 	JTextArea enterBet = new JTextArea();
+	// buttons to play the game
 	JButton enterBetBtn = new JButton("Enter Bet");
 	JButton hitBtn = new JButton("Hit");
 	JButton standBtn = new JButton("Stand");
-	Font font = new Font("Georgia", Font.PLAIN, 24);
-	JLabel background = new JLabel();
-	Image backgroundImg = Toolkit.getDefaultToolkit().getImage(BlackjackGUI.class.getResource("/background.png"));
+	// default font
+	Font font = new Font("Georgia", Font.PLAIN, 30);
+	
+	JLabel playerScore = new JLabel();
+	JLabel dealerScore = new JLabel();
+	
 	
 	public BlackjackGUI() {
 		initialize();
@@ -22,7 +30,7 @@ public class BlackjackGUI implements MouseMotionListener {
 	}
 	
 	private void initialize() {
-		frame.setBounds(150, 30, 1200, 800);
+		frame.setBounds(120, 20, 1200, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		panel.setBounds(0, 0, 1200, 800);
@@ -30,60 +38,84 @@ public class BlackjackGUI implements MouseMotionListener {
 		panel.addMouseMotionListener(this);
 		panel.setLayout(null);
 		
-		
 		setBet();
 		setHitBtn();
 		setStandBtn();
+		setScores();
 		setBackground();
 	}
 	
 	private void setBackground() {
+		Image backgroundImg = Toolkit.getDefaultToolkit().getImage(BlackjackGUI.class.getResource("/background.png"));
 		backgroundImg = backgroundImg.getScaledInstance(1200, 800, Image.SCALE_SMOOTH);
+		background.setForeground(Color.WHITE);
 		background.setIcon(new ImageIcon(backgroundImg));
 		background.setBounds(0, 0, 1200, 800);
 		panel.add(background);
 	}
 	
+	private void setScores() {
+		dealerScore.setForeground(Color.WHITE);
+		// call dealer/player score methods
+		dealerScore.setText("Dealer Score: ");
+		playerScore.setForeground(Color.WHITE);
+		playerScore.setText("Player Score: ");
+		
+		dealerScore.setBounds(456, 39, 224, 51);
+		dealerScore.setFont(font);
+		playerScore.setBounds(456, 582, 237, 51);
+		playerScore.setFont(font);
+		
+		panel.add(dealerScore);
+		panel.add(playerScore);
+	}
+	
 	private void setHitBtn() {
-		hitBtn.setBackground(Color.LIGHT_GRAY);
+		hitBtn.setForeground(Color.WHITE);
+		hitBtn.setBackground(Color.BLACK);
 		hitBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
 			}
 		});
 		hitBtn.addMouseMotionListener(this);
-		hitBtn.setBounds(357, 643, 142, 51);
+		hitBtn.setBounds(408, 643, 142, 51);
+		hitBtn.setFont(font);
 		panel.add(hitBtn);
 	}
 	
 	private void setStandBtn() {
-		standBtn.setBackground(Color.LIGHT_GRAY);
+		standBtn.setForeground(Color.WHITE);
+		standBtn.setBackground(Color.BLACK);
 		standBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
 			}
 		});
 		standBtn.addMouseMotionListener(this);
-		standBtn.setBounds(559, 643, 142, 51);
+		standBtn.setBounds(604, 643, 142, 51);
+		standBtn.setFont(font);
 		panel.add(standBtn);
 	}
 	
 	private void setBet() {
 		enterBet.setEditable(true);
 		enterBet.setWrapStyleWord(true);
-		enterBet.setBounds(61, 534, 152, 51);
+		enterBet.setBounds(61, 546, 170, 39);
 		panel.add(enterBet);
 		enterBet.setLineWrap(true);
 		enterBet.setFont(font);
+		enterBetBtn.setForeground(Color.WHITE);
 		
-		enterBetBtn.setBackground(Color.LIGHT_GRAY);
+		enterBetBtn.setBackground(Color.BLACK);
 		enterBetBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
 			}
 		});
 		enterBetBtn.addMouseMotionListener(this);
-		enterBetBtn.setBounds(61, 614, 152, 51);
+		enterBetBtn.setBounds(61, 614, 170, 51);
+		enterBetBtn.setFont(font);
 		panel.add(enterBetBtn);
 	}
 	
